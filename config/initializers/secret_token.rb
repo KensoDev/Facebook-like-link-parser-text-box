2e19bb1a6334be152f5d678412e9badcca1159ce
Avi Tzurel <avi@kensodev.com>
2011-07-12 22:40:27 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LinkParser::Application.config.secret_token = 'cf2af80186130496f3768b633489feec13730065896fae2726d0d26d44cc03028ca1d262469672e169c8dd09a1f56fcbcac0a2ac08d35d198d60069437da0db7'

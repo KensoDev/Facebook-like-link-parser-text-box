55bb7aa5bf02cb62338a696001e51961fb3fc547
Avi Tzurel <avi@kensodev.com>
2011-08-01 02:41:05 +0300

    Added the base class, added the lib spec

---
+++ lib/url_parser.rb
@@ -0,0 +1,21 @@
+class UrlParser
+  def initialize(url)
+    @url = url
+  end
+  
+  def parse_url    
+    
+  end
+  
+  def images
+    @images
+  end
+  
+  def title
+    @title
+  end
+  
+  def description
+    @description
+  end
+end
